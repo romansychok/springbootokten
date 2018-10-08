9c5ad51f3ff98e36302239b75f87d76c9e730509
romansychok <romansychok@gmail.com>
2018-10-09 00:46:06 +0300

    direction id user

--- src/main/java/ua/com/store/controller/UserController.java
+++ src/main/java/ua/com/store/controller/UserController.java
@@ -2,11 +2,17 @@ package ua.com.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ua.com.store.entity.User;
 import ua.com.store.service.UserService;
+
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -39,12 +45,47 @@ public class UserController {
         return "/userView/register";
     }
 
-    @PostMapping("/save")
-    public String save(@RequestParam("username") String username,
-                       @RequestParam("password") String password){
-        userService.save(new User(username,password));
-        return "redirect:/";
+//    @PostMapping("/save")
+//    public String save(@RequestParam("username") String username,
+//                       @RequestParam("password") String password){
+//        userService.save(new User(username,password));
+//        return "redirect:/";
+//
+//    }
 
+    @PostMapping("/save")
+    public String saveUser(User user , BindingResult result,
+                           @RequestParam("userImageX")MultipartFile multipartFile){
+        if (result.hasErrors()){
+            System.out.println("We have error!!!");
+            return "/userView/register";
+        }
+        String path = System.getProperty("user.home") + File.separator + "projectImages\\";
+
+        try {
+            multipartFile.transferTo(new File(path + multipartFile.getOriginalFilename()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("1111");
+        user.setUserImage("\\images\\" + multipartFile.getOriginalFilename());
+        userService.save(user);
+
+        return "/userView/register";
     }
+
+    @GetMapping("users")
+    public String users(Model model){
+        model.addAttribute("users",userService.findAll());
+        return "/userView/users";
+    }
+
+    @GetMapping("/user-{id}")
+    public String user(Model model,@PathVariable("id")int id){
+        Optional<User> one = userService.findOne(id);
+        model.addAttribute("user",one);
+        return "/userView/user";
+    }
+
 
 }
--- src/main/java/ua/com/store/service/ProductService.java
+++ src/main/java/ua/com/store/service/ProductService.java
@@ -13,4 +13,5 @@ public interface ProductService {
     Optional<Product> findOne(int id);
 
 
+
 }
